fd5d10555f463cae1c3051062b74cb2d219245ab
ggalmury <body0922@naver.com>
2025-05-29 01:18:16 +0900

    feat: ErrorBoundary fallback 컴포넌트 구현

--- src/app/(service)/insight/today/page.tsx
+++ src/app/(service)/insight/today/page.tsx
@@ -3,15 +3,17 @@ import { ErrorBoundary } from "react-error-boundary";
 import TodayInsightWelcomeMessage from "@/domains/insight/components/TodayInsightWelcomeMessage";
 import TodayInsightSection from "@/domains/insight/components/TodayInsightSection";
 
+import ErrorFallback from "@/components/organisms/ErrorFallback";
+
 const TodayInsightPage = () => {
   return (
-    <div className="space-y-10">
-      <TodayInsightWelcomeMessage />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div className="space-y-10">
+        <TodayInsightWelcomeMessage />
 
-      <ErrorBoundary fallback={<div>에러</div>}>
         <TodayInsightSection />
-      </ErrorBoundary>
-    </div>
+      </div>
+    </ErrorBoundary>
   );
 };
 
