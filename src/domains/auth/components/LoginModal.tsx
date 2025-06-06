90a3df4159c8b1f6e114c0cf14dbb9f3e69c5877
ggalmury <body0922@naver.com>
2025-06-06 16:46:05 +0900

    refactor: 로그인 모달 class 수정

--- src/domains/auth/components/LoginModal.tsx
+++ src/domains/auth/components/LoginModal.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import clsx from "clsx";
+
 import Kakao from "@/../public/svgs/kakao.svg";
 
 import { KAKAO_AUTH_URL } from "@/domains/auth/constants/url";
 
-import LoginButton from "@/components/atoms/button/LoginButton";
-import Heading1 from "@/components/atoms/typography/Heading1";
+import Title3 from "@/components/atoms/typography/Title3";
 import Body1 from "@/components/atoms/typography/Body1";
+import LoginButton from "@/components/atoms/button/LoginButton";
 
 interface LoginModalProps {
   isOpen: boolean;
@@ -19,12 +21,12 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
   }
 
   return (
-    <div onClick={onClose} className="flex items-center justify-center fixed z-50 inset-0 backdrop-blur-xs bg-service-black/30">
-      <div onClick={(e) => e.stopPropagation()} className="w-full max-w-sm px-8 py-6 space-y-10 rounded-2xl shadow-lg bg-white">
-        <div className="text-center space-y-4">
-          <Heading1 text="로그인" />
+    <div onClick={onClose} className="flex justify-center items-center fixed z-50 inset-0 backdrop-blur-xs bg-black/30">
+      <div className={clsx("flex flex-col w-xs p-6 gap-10 rounded-2xl shadow-lg bg-white", "md:w-sm")} onClick={(e) => e.stopPropagation()}>
+        <div className="flex flex-col text-center gap-4">
+          <Title3 text="로그인" />
 
-          <Body1 text="로그인하여 더 많은 기능을 이용해 보세요." styles={{ textColor: "text-service-gray" }} />
+          <Body1 text="로그인하여 더 많은 기능을 이용해 보세요." styles={{ color: "text-gray-400" }} />
         </div>
 
         <LoginButton
@@ -32,9 +34,7 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
           icon={<Kakao width={20} />}
           backgroundColor="bg-kakao-primary"
           foregroundColor="text-black"
-          onClick={() => {
-            window.location.href = KAKAO_AUTH_URL;
-          }}
+          onClick={() => (window.location.href = KAKAO_AUTH_URL)}
         />
       </div>
     </div>
