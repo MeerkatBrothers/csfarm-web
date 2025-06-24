1d5b72077c719b58a4e363df94cbd02f512e7259
ggalmury <body0922@naver.com>
2025-06-24 17:07:21 +0900

    feat: 애니메이션 추가

--- src/components/organisms/SubInsightSection.tsx
+++ src/components/organisms/SubInsightSection.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { motion, AnimatePresence, easeOut } from "framer-motion";
 
 import { ICON_SIZE } from "@/lib/constants/ui";
 
@@ -19,17 +20,30 @@ const SubInsightSection = ({ subInsight }: SubInsightSectionProps) => {
   const toggleDescription = (): void => setIsDescriptionOpen(!isDescriptionOpen);
 
   return (
-    <button className="flex flex-col text-start px-4 py-5 gap-4 rounded-lg bg-gray-100" onClick={toggleDescription}>
-      <div className="flex items-center justify-between w-full gap-4">
-        <div className="flex-1">
+    <div className="flex flex-col px-4 py-5 rounded-lg overflow-hidden bg-gray-100">
+      <button className="flex items-center justify-between w-full gap-4" onClick={toggleDescription}>
+        <div className="flex-1 text-start">
           <Body1 text={subInsight.subject} styles={{ weight: "font-bold" }} />
         </div>
 
         {isDescriptionOpen ? <FiChevronUp size={ICON_SIZE} /> : <FiChevronDown size={ICON_SIZE} />}
-      </div>
+      </button>
 
-      {isDescriptionOpen && <Body1 text={subInsight.description} reading />}
-    </button>
+      <AnimatePresence>
+        {isDescriptionOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            exit={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            transition={{ type: "spring", duration: 0.4, bounce: 0 }}
+          >
+            <div className="mt-2">
+              <Body1 text={subInsight.description} reading />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
